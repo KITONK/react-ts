313bfeb9fb501025dde8dde7188e277a041f97a3
KITONK <kiyj585@gmail.com>
2022-02-03 10:24:03 +0200

    init (props, events, router, hooks, requests)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
+import TodoItemPage from './components/TodoItemPage';
+import TodosPage from './components/TodosPage';
+import UserItemPage from './components/UserItemPage';
+import UserPage from './components/UserPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return ( 
+      <BrowserRouter>
+        <div>
+          <div>
+            <NavLink to="/users">Users</NavLink>
+            <NavLink to="/todos">Todos</NavLink>
+          </div>
+          <Route path={'/users'} exact>
+             <UserPage/>
+          </Route>
+          <Route path={'/todos'} exact>
+             <TodosPage/>
+          </Route>
+          <Route path={'/users/:id'}>
+             <UserItemPage/>
+          </Route>
+          <Route path={'/todos/:id'}>
+             <TodoItemPage/>
+          </Route>
+        </div>
+      </BrowserRouter>
   );
-}
+};
+
 
 export default App;
---
+++ src/components/UserItemPage.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useState, useEffect } from 'react';
+import { IUser } from '../types/types';
+import {useParams, useHistory} from 'react-router-dom';
+import axios from 'axios';
+
+interface UserItemPageParams {
+    id: string;
+}
+
+const UserItemPage: FC = () => {
+
+    const [user, setUser] = useState<IUser | null>(null)
+    const params = useParams<UserItemPageParams>()
+    const history = useHistory()
+  
+    useEffect(() => {
+      fetchUser();
+    }, [])
+  
+    async function fetchUser() {
+      try {
+        const responce = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users/' + params.id);
+        setUser(responce.data);
+      } catch (e) {
+        alert(e);
+      }
+    }
+
+  return (
+      <div>
+          <button onClick={()=>history.push('/users')}>Back</button>
+          <h1>Page user {user?.name}</h1>
+          <div>
+              {user?.email}
+          </div>
+          <div>
+              {user?.address.city} {user?.address.street} {user?.address.zipcode}
+          </div>
+      </div>
+  );
+};
+
+export default UserItemPage;
